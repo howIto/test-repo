18967afcbf3dd6a0d495fbfb68a9a384fda8b332
Ferret <aliveferret@gmail.com>
2016-06-20 21:52:57 +0300

    clone commit

---
+++ ActionBazaarCustom/src/main/java/com/actionbazaar/logic/metametameta.java
@@ -0,0 +1,24 @@
+package com.actionbazaar.logic;
+
+import javax.ejb.Local;
+
+@Local
+public interface BidService {
+
+	public void addBid(Bid bid);
+	public void addBid2(Bid2 bid2);
+	public void addBid2(Bid2 bid2);
+	public void addBid2(Bid2 bid2);
+	
+	branch master
+	branch master
+	branch master
+	
+	branch master
+	branch master
+	branch master
+	
+	
+	only meta file
+	
+}
